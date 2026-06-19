213fe1b3ee8d0a54a47d8e360b8a569f95284906
ChitranshDwivedi <dwivedichitransh1408@gmail.com>
2026-06-19 22:19:38 +0530

    Implement coin input for amount due in coke.py

---
+++ coke.py
@@ -0,0 +1,8 @@
+amount = 50
+while amount > 0:
+    print (f"Amount Due: {amount}")
+    coin = int(input("Insert Coin: ") )
+    if coin in [25,10,5]:
+        amount = amount - coin
+
+print(f"Change Owed: {amount * -1}")
